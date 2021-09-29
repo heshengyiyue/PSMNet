6a368d12341ec49f138da0461dca308952b96279
CHEN Jiahe <44491917+heshengyiyue@users.noreply.github.com>
2021-09-29 16:44:11 +0900

    Update batch_test_img.py

--- batch_test_img.py
+++ batch_test_img.py
@@ -2,14 +2,15 @@ import os
 
 left_img_path = '/mnt/c/users/public/images/rectified_laparoscope/rectified_left'
 right_img_path = '/mnt/c/users/public/images/rectified_laparoscope/rectified_right'
-output_path = '/mnt/c/Users/CHEN Jiahe/Documents/GitHub/PSMNet/models/pretrained_model_KITTI2015_results'
+output_path = '/mnt/c/users/chen\ jiahe/documents/gitHub/psmnet/models/pretrained_model_KITTI2015_results'
 
 left_img_list = os.listdir(left_img_path)
 right_img_list = os.listdir(right_img_path)
 
-for iter in len(left_img_list)
+for iter in range(len(left_img_list)):
     left_img_filename = left_img_list[iter]
     right_img_filename = right_img_list[iter]
-    ml =  'python Test_img.py --loadmodel ./models/pretrained_model_KITTI2015.tar --leftimg '\
-          + left_img_filename + ' --rightimg ' + right_img_filename + ' --outputpath ' + output_path
+    ml =  'python Test_img.py --loadmodel ./models/pretrained_model_KITTI2015.tar --leftimg ' \
+          + os.path.join(left_img_path, left_img_filename) + ' --rightimg ' + os.path.join(right_img_path, right_img_filename) \
+          + ' --outputpath ' + os.path.join(output_path, left_img_filename)
     os.system(ml)
