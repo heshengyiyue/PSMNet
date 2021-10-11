200dcd696b28feb54ae0071432147a54976315bd
CHEN Jiahe <44491917+heshengyiyue@users.noreply.github.com>
2021-10-11 22:04:59 +0900

    Update batch_test_img.py

--- batch_test_img.py
+++ batch_test_img.py
@@ -2,7 +2,7 @@ import os
 
 left_img_path = '/mnt/c/users/public/images/rectified_laparoscope/rectified_left'
 right_img_path = '/mnt/c/users/public/images/rectified_laparoscope/rectified_right'
-output_path = '/mnt/c/users/chen\ jiahe/documents/gitHub/psmnet/models/pretrained_model_KITTI2015_results'
+output_path = '/mnt/c/users/public/images'
 
 left_img_list = os.listdir(left_img_path)
 right_img_list = os.listdir(right_img_path)
