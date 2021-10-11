d62cd7c776f2a21afbfde3fa65c66109c7b9f945
CHEN Jiahe <44491917+heshengyiyue@users.noreply.github.com>
2021-10-11 22:14:57 +0900

    Update disparityPngToMat.m

--- disparityPngToMat.m
+++ disparityPngToMat.m
@@ -1,8 +1,8 @@
 %% PSMNet disparity.png to disparityMap (mat file) including color map
 extension = 'png';
-imgPath = ''; % original rectified image for the color map
-disparityPath = ''; % disparity.png path
-matPath = ''; % .mat path
+imgPath = 'C:\Users\Public\Images\rectified_laparoscope\rectified_left'; % original rectified image for the color map
+disparityPath = 'C:\Users\CHEN Jiahe\Documents\GitHub\PSMNet\models\pretrained_model_KITTI2015_results'; % disparity.png path
+matPath = [disparityPath '\mat']; % .mat path for output
 disparityList = dir([disparityPath '\*.' extension]);
 disparityNames = {disparityList.name};
 [disparityNames,~] = sortNat(disparityNames);
@@ -10,10 +10,10 @@ imgList = dir([imgPath '\*.' extension]);
 imgNames = {imgList.name};
 [imgNames,~] = sortNat(imgNames);
 for i = 1 : length(disparityList)
-    disparityPng = imread([disparityPath disparityNames{i}]);
+    disparityPng = imread([disparityPath '\' disparityNames{i}]);
     disparityPng = double(disparityPng);
     disparityMap = disparityPng/256;
-    img = imread([imgPath imgNames{i}]);
-    img = double(img);
-    save(strcat(matPath,'\',num2str(i),'.mat'))
+    img = imread([imgPath '\' imgNames{i}]);
+    colorMap = double(img);
+    save(strcat(matPath,'\',num2str(i),'.mat'),'colorMap','disparityMap');
 end
