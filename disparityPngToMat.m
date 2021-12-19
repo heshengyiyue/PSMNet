c64290815d476ed2e5359907110e9084798e3e6e
CHEN Jiahe <44491917+heshengyiyue@users.noreply.github.com>
2021-12-19 15:22:59 +0900

    Update disparityPngToMat.m

--- disparityPngToMat.m
+++ disparityPngToMat.m
@@ -1,7 +1,7 @@
 %% PSMNet disparity.png to disparityMap (mat file) including color map
 extension = 'png';
-imgPath = 'C:\Users\Public\Images\rectified_laparoscope\rectified_left'; % original rectified image for the color map
-disparityPath = 'C:\Users\CHEN Jiahe\Documents\GitHub\PSMNet\models\pretrained_model_KITTI2015_results'; % disparity.png path
+imgPath = 'C:\Users\Public\Images\rectified_fabric\rectified_textureMask\left'; % original rectified image for the color map
+disparityPath = 'C:\Users\Public\Images\rectified_fabric\disparityMap'; % disparity.png path
 matPath = [disparityPath '\mat']; % .mat path for output
 disparityList = dir([disparityPath '\*.' extension]);
 disparityNames = {disparityList.name};
